ed4354db5047dd322a0a926d55d95b36f2575cb9
Rafaqat Ahmad <irahilabassi@gmail.com>
2022-06-09 02:40:56 +0530

    First Commit

---
+++ src/components/contexts/AuthContext.jsx
@@ -0,0 +1,16 @@
+
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState("Login");
+  const handleUserStatus = () => {
+    setIsAuth(isAuth === "Login" ? "Logout" : "Login");
+  };
+  return (
+    <AuthContext.Provider value={{ isAuth, handleUserStatus }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
